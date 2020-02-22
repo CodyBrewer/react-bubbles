ab6d499667cd6c6bc7c8e9c895c66ad97c341822
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-22 10:18:09 -0700

    PrivateRoute component created in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -16,6 +16,19 @@ function App() {
       </div>
     </Router>
   );
+}
+
+function PrivateRoute({ component: Component, ...rest }) {
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />;
 }
 
 export default App;
